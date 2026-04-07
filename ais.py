49f44e47879bad0696d2249937680a3c6566fe63
Matthew-Dam <dampteymatthew13579@gmail.com>
2026-04-07 23:16:45 +0000

    Add AIs class with AI URLs

---
+++ ais.py
@@ -0,0 +1,8 @@
+class AIs:
+    def __init__(self):
+        self.AI_URLS = {"ChatGPT": "https://chat.openai.com",
+                        "Gemini": "https://gemini.google.com",
+                        "Claude": "https://claude.ai",
+                        "Copilot": "https://copilot.microsoft.com",
+                        "Perplexity": "https://perplexity.ai"}
+
